6d10f619ad68a4249bc0e21718da36320c89c535
Wilson Leao <wln1987@gmail.com>
2023-01-05 22:23:20 +0100

    452. Minimum Number of Arrows to Burst Balloons

---
+++ 452.py
@@ -0,0 +1,25 @@
+#
+# 452. Minimum Number of Arrows to Burst Balloons
+# https://leetcode.com/problems/minimum-number-of-arrows-to-burst-balloons
+#
+class Solution:
+
+    def findMinArrowShots(self, points: list[list[int]]) -> int:
+        # xstart < xend
+        points = sorted(points, key=lambda p: p[1])
+        # -2**31 <= xstart < xend <= 2**31 - 1
+        last_x = -2**31 - 1
+        arrows = 0
+        for xs, xe in points:
+            if xs > last_x:
+                last_x = xe
+                arrows += 1
+
+        return arrows
+
+
+if __name__ == "__main__":
+    sol = Solution()
+    assert 2 == sol.findMinArrowShots([[10,16],[2,8],[1,6],[7,12]])
+    assert 4 == sol.findMinArrowShots([[1,2],[3,4],[5,6],[7,8]])
+    assert 2 == sol.findMinArrowShots([[1,2],[2,3],[3,4],[4,5]])
